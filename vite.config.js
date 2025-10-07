b9fa5802f5bba1bb42b30f95d54a6617887b66c8
maninthebox235 <129606663+maninthebox235@users.noreply.github.com>
2025-10-07 18:31:02 +0000

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/codespaces-react-1/',
   test: {
     globals: true,
     environment: 'jsdom',
